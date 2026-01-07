31ca705d9f3e98c544f06a7d7503d10026e28dac
Muhammad Asad <113167175+MuhammadAsad00@users.noreply.github.com>
2026-01-07 21:13:42 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,50 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/db.js';
-import authRoute from './routes/authRoute.js';
-import userRoute from './routes/userRoute.js';
-import productRoute from './routes/productRoute.js';
-import cartRoute from './routes/cartRoute.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
+import productRoute from "./routes/productRoute.js";
+import cartRoute from "./routes/cartRoute.js";
 
 dotenv.config();
-let port = process.env.PORT || 3000;
+
+const app = express();
 
 app.set("trust proxy", 1);
-const app = express();
 app.use(cookieParser());
-
 app.use(express.json());
-app.use(cors({
-  origin: [
-    "https://ecommerce-fullstack-design-client.vercel.app",
-    "https://ecommerce-fullstack-design-admin.vercel.app"
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-}));
 
+app.use(
+  cors({
+    origin: [
+      "https://ecommerce-fullstack-design-client.vercel.app",
+      "https://ecommerce-fullstack-design-admin.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+// Preflight
 app.options("*", cors({
   origin: [
     "https://ecommerce-fullstack-design-client.vercel.app",
-    "https://ecommerce-fullstack-design-admin.vercel.app"
+    "https://ecommerce-fullstack-design-admin.vercel.app",
   ],
-  credentials: true
+  credentials: true,
 }));
 
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/product", productRoute);
 
-app.use('/api/auth', authRoute);
-app.use('/api/user', userRoute);
-app.use('/api/cart', cartRoute);
-app.use('/api/product', productRoute);
-
-
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    connectDB();
-});
+connectDB();
 
 export default app;
